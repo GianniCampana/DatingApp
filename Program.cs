9f53db46d5d47beee63becf2e1ad644513a381cd
GianniCampana <campanagianni97@gmail.com>
2023-04-01 10:38:39 +0200

    Sistemazione CORS Policy

--- Program.cs
+++ Program.cs
@@ -11,12 +11,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+//aggiungo l'AddCorsa per il dire al browser di fidarsi del server
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+//in questo punto sempre per il problema della Cors aggiungo questa riga di codice che dice che va bene qualsiasi header di qualsiasi metodo di qualsiasi origine
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.MapControllers();
 
 app.Run();
